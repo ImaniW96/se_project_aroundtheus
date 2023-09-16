df6cfff2b0e7e278791cef4ceed0a640814d8070
ImaniW96.git <imaniwalker0305@gmail.com>
2023-09-16 03:37:27 -0400

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -34,5 +34,5 @@ const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 
 profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal_opened");
+  profileEditModal.classList.add("modal__opened");
 });
