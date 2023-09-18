2a1bbf085ceaa98e7153917a2b413eae3541a410
ImaniW96.git <imaniwalker0305@gmail.com>
2023-09-17 22:05:25 -0400

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -39,9 +39,12 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector('.card-title')
 
 function closePopop() {
   profileEditModal.classList.remove("modal__opened");
+}
+function getCardElement(cardData){
 }
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
@@ -60,4 +63,13 @@ profileEditForm.addEventListener("submit", (e) => {
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
   closePopop();
+});
+initialCards.forEach((cardData) => {
+  const cardTemplate = cardElement.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card-image");
+  const cardTitleEl = cardElement.querySelector(".card-title");
+  const cardTitleEl.textContent= cardData.name;
+  return cardElement
+  const cardElement = getCardElement(cardData)
+  cardListEl.prepend(cardElement)
 });
