4c5042af9ec82db97fa2d1a1f387712c8cbc5500
ImaniW96.git <imaniwalker0305@gmail.com>
2024-01-04 23:42:06 -0500

    npm update

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,6 @@
+// data should be an object containing the name and link
+// open(data) {
+//     // set the image's src and alt
+//     // set the caption's textContent
+//     super.open();
+//   }
