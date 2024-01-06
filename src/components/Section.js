2c2f6ac63336a8a6610b2101e5a1a9d22e7014f7
ImaniW96.git <imaniwalker0305@gmail.com>
2024-01-06 07:12:47 -0500

    Section update

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,1 +1,11 @@
-items, render;
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+  renderItems() {}
+  addItem(element) {
+    this._container.prepend(element);
+  }
+}
