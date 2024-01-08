774ae20e7306cda30af86c69b8e426c5b4474477
ImaniW96.git <imaniwalker0305@gmail.com>
2024-01-08 07:09:38 -0500

    Index js update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -30,6 +30,11 @@ const cardData = {
   name: "Yosemite Valley",
   link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
 };
+const section = new Section(
+  { items: initialCards, renderer: () => {} },
+  ".card__list"
+);
+section.renderItems();
 //Elements
 
 const profileEditButton = document.querySelector("#profile-edit-button");
