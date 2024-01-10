2a9b96114da8e9e2f9a4a17c6d1e52fa717de81d
ImaniW96.git <imaniwalker0305@gmail.com>
2024-01-10 04:23:08 -0500

    Update

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,8 +2,22 @@ class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
   }
-  open() {}
-  close() {}
+  open() {
+    function openPopup(modal) {
+      document.addEventListener("keydown", closeModalByEscape);
+      modal.classList.add("modal_opened");
+    }
+  }
+  close() {
+    function closeModalByEscape(event) {
+      if (event.key === "Escape") {
+        const modalOpened = document.querySelector(".modal_opened");
+        if (modalOpened) {
+          closePopup(modalOpened);
+        }
+      }
+    }
+  }
   _handleEscapeClose() {}
   setEventListeners() {}
 }
