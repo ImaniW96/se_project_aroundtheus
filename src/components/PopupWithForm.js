6df3d387dd8495118817bda2c488e3274f6fad06
ImaniW96.git <imaniwalker0305@gmail.com>
2024-01-14 07:50:37 -0500

    Update

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,11 +1,14 @@
 import Popup from "./Popup.js";
-class PopupWithForm extends Popup{
-    constructor(popupSelector, handleFormSubmit){
-        super({popupSelector});
-        this._popupForm = this._popupElement.querySelector(".popup__form");
-        this._handleFormSubmit = handleFormSubmit;
-    }
-    close(){
-        this.popupForm.reset()
-    }
+class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super({ popupSelector });
+    this._popupForm = this._popupElement.querySelector(".popup__form");
+    this._handleFormSubmit = handleFormSubmit;
+  }
+  _getInputValues() {
+    const input = this._popupForm.querySelectorAll("input");
+  }
+  close() {
+    this.popupForm.reset();
+  }
 }
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,3 +4,9 @@
 //     // set the caption's textContent
 //     super.open();
 //   }
+
+function handlePopupWithImage(imageSector, title, link, textContent) {
+  const imageSeletor = imageSelector;
+  const newImagePopup = ImagePopup(imageSector);
+  this._cardImage = this._modal.querySelector("modal__image");
+}
