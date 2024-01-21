ff31b802ceaf6c111fc66b74302ed2cb75a7b227
ImaniW96.git <imaniwalker0305@gmail.com>
2024-01-21 07:29:42 -0500

    User info update

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,17 +1,14 @@
-export default class Userinfo{
-    constructor(profileTitle, profileDesription ){
-        this._name = profileTitle;
-        this._description = profileDesription;
-    }
-        getUserInfo(){
-    return this._name , this._description
-        }
-         
-            
-        setUserInfo(){
-        };
-      
-    };
-    
+export default class Userinfo {
+  constructor(profileTitle, profileDesription) {
+    this._name = profileTitle;
+    this._description = profileDesription;
+  }
+  getUserInfo() {
+    return this._name, this._description;
+  }
 
- 
+  setUserInfo(title, description) {
+    this._profiletitleElement.textContent = title;
+    this._profileDescriptionElement.textContent = description;
+  }
+}
