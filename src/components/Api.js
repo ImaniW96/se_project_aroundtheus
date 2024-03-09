ab2db6e85ccd3220f90bee13bacfdd321de46bf9
ImaniW96.git <imaniwalker0305@gmail.com>
2024-03-08 23:51:56 -0500

    update

--- src/components/Api.js
+++ src/components/Api.js
@@ -15,15 +15,47 @@ class Api {
     });
   }
   createCards() {
-    //
+    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+      headers: {
+        authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    });
   }
   likeCards() {
-    //
+    return fetch("https://around-api.en.tripleten-services.com/v1/like", {
+      headers: {
+        authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    });
   }
   deleteCards() {
-    //
+    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+      headers: {
+        authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    });
   }
   dislikeCards() {
-    //
+    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+      headers: {
+        authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    });
   }
 }
