5d23039152ce52207a5914dca102f8985755718d
ImaniW96.git <imaniwalker0305@gmail.com>
2024-03-09 19:21:00 -0500

    update

--- src/components/Api.js
+++ src/components/Api.js
@@ -4,7 +4,8 @@ class Api {
   }
 
   getInitialCards() {
-    return fetch("https://around-api.en.tripleten-services.com/v1/get/cards", {
+    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+      method: "GET",
       headers: {
         authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
       },
@@ -14,8 +15,9 @@ class Api {
       }
     });
   }
-  createCards() {
-    return fetch("https://around-api.en.tripleten-services.com/v1/post/cards", {
+  createCard() {
+    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+      method: "POST",
       headers: {
         authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
       },
@@ -27,6 +29,7 @@ class Api {
   }
   likeCards() {
     return fetch("https://around-api.en.tripleten-services.com/v1/like", {
+      method: "POST",
       headers: {
         authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
       },
@@ -37,28 +40,24 @@ class Api {
     });
   }
   deleteCards() {
-    return fetch(
-      "https://around-api.en.tripleten-services.com/v1/delete/cards",
-      {
-        headers: {
-          authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
-        },
-      }
-    ).then((res) => {
+    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+      method: "DELETE",
+      headers: {
+        authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
+      },
+    }).then((res) => {
       if (res.ok) {
         return res.json();
       }
     });
   }
   dislikeCards() {
-    return fetch(
-      "https://around-api.en.tripleten-services.com/v1/delete/likes",
-      {
-        headers: {
-          authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
-        },
-      }
-    ).then((res) => {
+    return fetch("https://around-api.en.tripleten-services.com/v1/likes", {
+      method: "DELETE",
+      headers: {
+        authorization: "e3f5bc64-c279-4474-9c65-8c5ae0831eb9",
+      },
+    }).then((res) => {
       if (res.ok) {
         return res.json();
       }
